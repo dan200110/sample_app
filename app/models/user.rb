9e5d3180ee2cfdc8868fe6b384bc81a0954e0eda
Your Name <Dan123456dz@gmail.com>
2022-06-29 13:52:34 +0700

    Chapter 7: Sign up

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,16 +1,17 @@
 class User < ApplicationRecord
+  USER_ATTRS = %w(name email password password_confirmation).freeze
   before_save :downcase_email
 
   validates :email, presence: true,
-    length: Settings.user.email.email_length_range,
-    format: {with: Settings.user.valid_email_regex},
+    length: {minimum: Settings.user.email.email_min_length},
+    format: {with: Settings.user.VALID_EMAIL_REGEX},
     uniqueness: true
 
   validates :name, presence: true,
     length: {maximum: Settings.user.name_validates.name_max_length}
 
   validates :password, presence: true,
-    length: {minimum: Settings.user.password_validates.password_min_lenght}, if: :password
+    length: {minimum: Settings.user.password_validates.password_min_length}, if: :password
 
   has_secure_password
 

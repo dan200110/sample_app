c2061874ce3429d72a46d190099e67a4ccf17538
Your Name <Dan123456dz@gmail.com>
2022-07-01 15:18:52 +0700

    Chapter 10: Update, showing and deleting users

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,6 +9,9 @@ import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 require("jquery")
 import "bootstrap"
+
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+
+require ("pagy/extras/bootstrap")
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_28_012150) do
+ActiveRecord::Schema.define(version: 2022_07_04_014345) do
 
   create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 2022_06_28_012150) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "remember_digest"
+    t.boolean "admin"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
